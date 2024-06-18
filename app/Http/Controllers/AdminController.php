d4f6cc5dbf04939983dd99c29de498ef1d24009d
SISTEMASAUGE12 <sistemas@educaauge.com>
2024-06-17 21:17:38 -0500

    OKIS

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Credito;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -10,5 +11,31 @@ class AdminController extends Controller
     public function index(){
         $usuarios = User::all();
         return view('admin.index',['usuarios'=>$usuarios]);
+    }
+
+    public function aprobar(Request $request){
+        $credito = Credito::find($request->id);
+        $credito->estado = 'aprobado';
+        $credito->comentario_asesor = $request->comentario;
+        $credito->save();
+
+        return response()->json([
+            'redirect' => route('creditos.index'),
+            'mensaje' => 'El crédito ha sido aprobado correctamente',
+            'icono' => 'success'
+        ]);
+    }
+
+    public function rechazar(Request $request){
+        $credito = Credito::find($request->id);
+        $credito->estado = 'rechazado';
+        $credito->comentario_asesor = $request->comentario;
+        $credito->save();
+
+        return response()->json([
+            'redirect' => route('creditos.index'),
+            'mensaje' => 'El crédito ha sido rechazado correctamente',
+            'icono' => 'success'
+        ]);
     }
 }
