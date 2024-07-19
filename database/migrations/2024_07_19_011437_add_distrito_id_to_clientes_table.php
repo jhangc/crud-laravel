4bbbbc2374f8007449420f7748706b307c143369
jhangc <gcjhan2001@gmail.com>
2024-07-19 03:51:00 -0500

    dsdsa

--- database/migrations/2024_07_19_011437_add_distrito_id_to_clientes_table.php
+++ database/migrations/2024_07_19_011437_add_distrito_id_to_clientes_table.php
@@ -10,14 +10,12 @@ class AddDistritoIdToClientesTable extends Migration
     {
         Schema::table('clientes', function (Blueprint $table) {
             $table->unsignedBigInteger('distrito_id')->nullable()->after('direccion');
-            $table->foreign('distrito_id')->references('dis_id')->on('distritos')->onDelete('set null');
         });
     }
 
     public function down()
     {
         Schema::table('clientes', function (Blueprint $table) {
-            $table->dropForeign(['distrito_id']);
             $table->dropColumn('distrito_id');
         });
     }
